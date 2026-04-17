495207932db31bba8b77bfce1c3152a5223cf139
Elad Avron <eladavron@gmail.com>
2026-04-17 13:01:28 +0300

    Added Color Adjustment to dials

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
 import { PluginSDK } from '@logitech/plugin-sdk';
 import { ToggleAction } from './src/toggle-action';
 import { BrightnessAdjust } from './src/brightness-adjust';
+import { ColorAdjust } from './src/color-adajust';
 
 const pluginSDK = new PluginSDK();
 
 // Register plugin actions
 pluginSDK.registerAction(new ToggleAction());
 pluginSDK.registerAction(new BrightnessAdjust());
+pluginSDK.registerAction(new ColorAdjust());
 
 await pluginSDK.connect();
---
+++ src/color-adajust.ts
@@ -0,0 +1,40 @@
+import { AdjustmentAction } from '@logitech/plugin-sdk';
+import { findDevices,  getMaximumTemperatureInKelvinForDevice, getMinimumTemperatureInKelvinForDevice, getTemperatureInKelvin, setTemperatureInKelvin } from 'litra';
+
+function adjustBrightness(tick: number) {
+    console.log('Adjusting brightness of the lights');
+    for (const light of findDevices()) {
+        const currentTemperature = getTemperatureInKelvin(light);
+        const maxTemperature = getMaximumTemperatureInKelvinForDevice(light);
+        const minTemperature = getMinimumTemperatureInKelvinForDevice(light);
+        const currentTemperaturePercentage = ((currentTemperature - minTemperature) / (maxTemperature - minTemperature)) * 100;
+        let newTemperaturePercentage;
+        let newTemperatureInKelvin;
+        if (tick > 0) { // Increase temperature
+            newTemperaturePercentage = Math.min(currentTemperaturePercentage + tick, 100);
+            newTemperatureInKelvin = minTemperature + (newTemperaturePercentage / 100) * (maxTemperature - minTemperature);
+        } else if (tick < 0) {  // Decrease temperature
+            newTemperaturePercentage = Math.max(currentTemperaturePercentage + tick, 0);
+            newTemperatureInKelvin = minTemperature + (newTemperaturePercentage / 100) * (maxTemperature - minTemperature);
+        }
+        else {
+            console.log(`Temperature of light with serial number ${light.serialNumber} remains unchanged at ${currentTemperaturePercentage}%`);
+            continue; // No change in temperature
+        }
+
+        newTemperatureInKelvin = Math.round(newTemperatureInKelvin / 100) * 100;
+        console.log(`Setting temperature of light with serial number ${light.serialNumber} to ${newTemperaturePercentage}% (${newTemperatureInKelvin}K rounded to nearest 100)`);
+        setTemperatureInKelvin(light, newTemperatureInKelvin);
+    }
+}
+
+export class ColorAdjust extends AdjustmentAction {
+    hasReset = false;
+    name = 'Adjust_Color';
+    displayName = 'Color Adjustment';
+    description = 'Adjust the color of the lights';
+
+    execute(event: { tick: number; }) {
+        adjustBrightness(event.tick);
+    }
+}
